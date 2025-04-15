dc2b3509d8ab4985ca6283d5cdd8b4dc4cf2a1f0
Tidu09 <45685841+Tidu09@users.noreply.github.com>
2025-04-15 13:04:21 +0530

    Update shiprocket-order.js

--- api/shiprocket-order.js
+++ api/shiprocket-order.js
@@ -1,5 +1,17 @@
 import nodemailer from 'nodemailer';
 
+const allowedOrigin = "https://kriyaedu.com";
+
+if (req.method === 'OPTIONS') {
+  res.setHeader('Access-Control-Allow-Origin', allowedOrigin);
+  res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  return res.status(200).end();
+}
+
+res.setHeader('Access-Control-Allow-Origin', allowedOrigin);
+
+
 export default async function handler(req, res) {
   if (req.method !== 'POST') {
     return res.status(405).json({ error: 'Method not allowed' });
